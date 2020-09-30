0db068080d1ad395e9369faed0dabe761e9d5077
d4nd14z <newdmdiazf@gmail.com>
2020-09-30 17:29:37 -0500

    Se agrega la extension php-cli a PHP

--- full-lamp.sh
+++ full-lamp.sh
@@ -58,7 +58,7 @@ sudo mysql
 # Instalacion de CURL
 sudo apt update && sudo apt install curl
 #Instalacion de PHP
-sudo apt update && sudo apt install php libapache2-mod-php php-mysql php-curl
+sudo apt update && sudo apt install php libapache2-mod-php php-mysql php-curl php-cli
 #Testear la instalacion de PHP desde la consola
 php -version
 
